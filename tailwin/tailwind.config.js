8e3b489ed2af174af5882f3cb1d896661d6ad9f3
Avanish1971 <avanishdhake1@gmail.com>
2025-07-29 23:33:24 +0530

    paytm_cloning

---
+++ tailwin/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        nilla:"#00baf2",
+        darknilla:"#002970",
+        lightColor:"#f5f5f5",
+        lightnilla:"#00afe3",
+        lightdarknilla:"#0f4aBa",
+        bgColor:"#f5f7fa",
+      }
+    },
+  },
+  plugins: [],
+}
+
